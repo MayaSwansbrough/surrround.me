b5145725f1376d846b65bd002baa15e2c0347717
Maya Swansbrough <maya.swansbrough@gmail.com>
2023-12-27 12:30:34 -0500

    Searchbar setup

--- frontend/src/components/Searchbar/Searchbar.tsx
+++ frontend/src/components/Searchbar/Searchbar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
 import { Statuses, fetchArtistsAsync, selectArtists, selectStatus } from './searchbarSlice';
 import Box from '@mui/material/Box';
@@ -11,48 +11,57 @@ interface Props {
 }
  
 const Searchbar: React.FC<Props> = () => {
-  const artists = useAppSelector(selectArtists);
-  const status = useAppSelector(selectStatus);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(fetchArtistsAsync());
   }, [dispatch])
 
-  let contents;
+  const [artistNameQuery, setArtistNameQuery] = useState<string>(''); // State to store the search query
 
-  if (status !== Statuses.UpToDate) {
-    contents = <div>{status}</div>
-  } else {
-    contents = <div className="card">
-      <div className="card-body">
-        <h3>{status}</h3>
-      </div>
-    </div>
-  }
+  const search = (artistName: string) => {
+    // Your search logic here, using the provided query
+    console.log('Searching for:', artistName);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setArtistNameQuery(event.target.value);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    // Check if the Enter key is pressed (key code 13)
+    if (event.key === 'Enter') {
+      // Prevent the default behavior of the Enter key (e.g., form submission)
+      event.preventDefault();
+
+      // Call your search function with the entered value
+      search(artistNameQuery);
+    }
+  };
 
   return (
-    <div><h3>Artists</h3>
-      {contents}
-      <Box
-        component="form"
-        sx={{
-          '& .MuiTextField-root': { m: 1, width: '75%' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <div>
-        <TextField
-            className="searchField"
-            id="filled-search"
-            label="Artist Name"
-            type="search"
-            variant="outlined"
-          />
-        </div>
-      
-      </Box>
+    <div className="searchBar">
+      <div className="searchBar-container">
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '75%' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+              className="searchBar-container-field"
+              id="filled-search"
+              label="Artist Name"
+              type="search"
+              variant="outlined"
+              value={artistNameQuery}
+              onChange={handleChange}
+              onKeyDown={handleKeyDown}
+            />
+        </Box>
+      </div>
     </div>
   )
 }
